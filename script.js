d75add59a9dbe6e247b6ae62c6c437283f5c656e
Gunner <86052548+RegCoDing2@users.noreply.github.com>
2023-08-03 08:17:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,265 @@
+let displayValue = "";
+let input1;
+let input2;
+let operator;
+let operatorClicked = false;
+let equalClicked = false;
+let chain = false;
+
+const display = document.querySelector(".display");
+
+const btnClear = document.querySelector(".clear");
+
+btnClear.addEventListener('click', function(e){
+    displayValue = null;
+    input1 = null;
+    input2 = null;
+    operator = "";
+    operatorClicked = false;
+    equalClicked = false;
+    chain = false;
+    display.innerText = "";
+});
+
+document.addEventListener('keydown', function(event) {
+    // Check if the key pressed is a number or the backspace key
+    if (event.key >= '0' && event.key <= '9') 
+    {
+        if(chain == true)
+        {
+            display.innerText = "";
+
+            if(display.innerText.length < 9)
+            {
+                display.innerText += event.key;
+            }
+
+            chain = false;
+        }
+        else
+        {
+            if(display.innerText.length < 9)
+            {
+                display.innerText += event.key;
+            }
+        }
+    }
+    if(event.key == 'Backspace')
+    {
+        displayValue = display.innerText;
+
+        display.innerText = displayValue.slice(0, -1);
+    }
+  });
+  
+
+const btnNum = document.querySelectorAll(".number");
+
+btnNum.forEach((numberButton) => {
+    numberButton.addEventListener('click', function(e) {
+        if(chain == true)
+        {
+            display.innerText = "";
+
+            if(display.innerText.length < 9)
+            {
+                display.innerText += e.target.value;
+            }
+
+            chain = false;
+        }
+        else
+        {
+            if(display.innerText.length < 9)
+            {
+                display.innerText += e.target.value;
+            }
+        }
+    });
+});
+
+const btnOperator = document.querySelectorAll(".operator");
+
+btnOperator.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', function(e) {
+
+    if(display.innerText.toString() == "")
+    {
+
+    }
+    else
+    {
+        if(operatorClicked == false)
+        {
+            input1 = parseFloat(display.innerText);
+
+            operator = e.target.value;
+
+            display.innerText = "";
+
+            operatorClicked = true;
+
+            changeOperator = true;
+
+            equalClicked = false;
+        }
+        else if(operatorClicked == true)   //For chaining purposes
+        {
+            input2 = parseFloat(display.innerText);
+
+            displayValue = operate(input1, input2, operator);
+            
+            if (displayValue.toString().length > 9) 
+            {
+                displayValue = parseFloat(displayValue.toFixed(9));
+            }
+
+            display.innerText = displayValue;
+
+            input1 = parseFloat(display.innerText); 
+
+            operator = e.target.value;
+
+            equalClicked = false;  
+
+            chain = true;
+        }
+    }
+    });
+});
+
+const btnEquals = document.querySelector(".equals");
+
+btnEquals.addEventListener('click', function(e){
+    if(display.innerText.toString() == "")
+    {
+        
+    }
+    else
+    {
+        //--------------------------------Normal---------------------------------
+        if(equalClicked == false)
+        {
+            input2 = parseFloat(display.innerText);
+
+            displayValue = operate(input1, input2, operator);
+            
+            if (displayValue.toString().length > 9) 
+            {
+                displayValue = parseFloat(displayValue.toFixed(9));
+            }
+
+            display.innerText = displayValue;
+
+            input1 = parseFloat(display.innerText);
+
+            operatorClicked = false;
+
+            changeOperator = false;
+
+            equalClicked = true;
+        }
+        else    //----------------------Chain--------------------------------------
+        {
+            display.innerText = "";
+
+            displayValue = operate(input1, input2, operator);
+            
+            if (displayValue.toString().length > 9) 
+            {
+                displayValue = parseFloat(displayValue.toFixed(9));
+            }
+
+            display.innerText = displayValue;
+
+            input1 = parseFloat(display.innerText);
+        }
+    }
+});
+
+const btnSign = document.querySelector(".sign");
+
+btnSign.addEventListener('click', function(e){
+    if(display.innerText.toString() == "")
+    {
+
+    }
+    else
+    {
+       display.innerText = parseFloat(display.innerText) * -1; 
+    }
+    
+});
+
+const btnPercent = document.querySelector(".percent");
+
+btnPercent.addEventListener('click', function(e){
+    if(display.innerText.toString() == "")
+    {
+
+    }
+    else
+    {  
+        displayValue = parseFloat(display.innerText)/100;
+            
+        if (displayValue.toString().length > 9) 
+        {
+            displayValue = parseFloat(displayValue.toFixed(9));
+        }
+
+        display.innerText = displayValue;
+    }
+    
+});
+
+const btnDecimal = document.querySelector(".decimal");
+
+btnDecimal.addEventListener('click', function(e){
+    if(display.innerText.toString().includes(".") == false && display.innerText.toString() != "")
+    {
+        display.innerText = display.innerText.toString() + '.';
+    }
+});
+
+function add(number1, number2){
+    let result = parseFloat(number1 + number2);
+
+    return result;
+}
+
+function subtract(number1, number2){
+    let result = parseFloat(number1 - number2);
+
+    return result;
+}
+
+function multiply(number1, number2){
+    let result = parseFloat(number1 * number2);
+
+    return result;
+}
+
+function divide(number1, number2){
+    let result = parseFloat(number1 / number2);
+
+    return result;
+}
+
+function operate(number1, number2, operator){
+    if(operator == '+')
+    {
+        return add(number1, number2);
+    }
+    else if(operator == '-')
+    {
+        return subtract(number1, number2);
+    }
+    else if(operator == 'x')
+    {
+        return multiply(number1, number2);
+    }
+    else if(operator == '/')
+    {
+        return divide(number1, number2);
+    }
+}
